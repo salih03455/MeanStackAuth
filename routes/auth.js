724ee7bb394d0479da434c753a9d8a9550edc936
salih03455 <salih03455@gmail.com>
2020-10-18 01:13:56 +0300

    login apisi olusturuldu, token uretildi

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,12 @@
 const router = require('express').Router();
 const User = require('../model/User');
 const bcrypt = require('bcryptjs');
-
-router.post('/login', async (req, res, next) => {
-
-});
+const jwt = require('jsonwebtoken');
 
 router.post('/signup', async (req, res, next) => {
-  // Email veritabaninda mevcut mu?
-  const emailExist = await User.findOne({ email: req.body.email })
-  if (emailExist) return res.status(400).send('Email already exist!')
+  // Bu email ile kayitli bir kullanici (userData) mevcut mu?
+  const userData = await User.findOne({ email: req.body.email })
+  if (userData) return res.status(400).send('Email already exist!')
   
   // Parolayi sifrele:
   const salt = await bcrypt.genSalt(10);
@@ -23,11 +20,34 @@ router.post('/signup', async (req, res, next) => {
   });
 
   try {
+    // Olusturulan User objesini veritabanina kaydet:
     const savedUser = await user.save();
     res.send(savedUser);
   } catch (err) {
     res.status(400).send(err);
   }
+});
+
+router.post('/login', async (req, res, next) => {
+  // Bu email ile kayitli bir kullanici (userData) mevcut mu?
+  const userData = await User.findOne({ email: req.body.email });
+  if (!userData) return res.status(400).json({
+    message: 'Email is not found!'
+  });
+
+  // Parola dogru mu?
+  const validPass = await bcrypt.compare(req.body.password, userData.password)
+  if (!validPass) return res.status(400).send('Invalid password!');
+
+  // Token olustur:
+  const token = jwt.sign(
+    { _id: userData._id },
+    process.env.TOKEN_SECRET,
+    { expiresIn: '1h' }
+  );
+
+  // Token'i responsenin headerine ekle:
+  res.header('auth-token', token).send(token);
 });
 
 module.exports = router;
