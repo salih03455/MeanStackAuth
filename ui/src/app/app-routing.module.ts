89fe3aaf811365a7dfce9d81c13300dec9d22431
Salih Pehlivan <salihpehlivan@Salihs-MacBook-Pro.local>
2020-10-25 22:18:58 +0300

    router guard eklendi

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { PostsComponent } from './posts/posts.component';
@@ -7,11 +8,12 @@ import { PostsComponent } from './posts/posts.component';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'posts', component: PostsComponent }
+  { path: 'posts', component: PostsComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
